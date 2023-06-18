d2b3c4ca97bb5f2d02358758b4b1697a5f94b5f6
Iwongeli <117666577+Iwongeli@users.noreply.github.com>
2023-06-18 17:34:48 +0000

    nike.py

--- shoeup/website_scrapers/nike.py
+++ shoeup/website_scrapers/nike.py
@@ -1,11 +1,10 @@
 # NIKE API CALL
 import requests
 import json
-import pandas as pd
 from time import sleep
 
 def callAPI(url):
-    html = requests.get(url=url)
+    html = requests.get(url=url, timeout=3)
     output = json.loads(html.text)
     return output
 
@@ -30,7 +29,7 @@ def main():
     country_language = 'pl'
     query = 'jordan'
     url = f'https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=241B0FAA1AC3D3CB734EA4B24C8C910D&country={country}&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace({country})%26filter%3Dlanguage({country_language})%26filter%3DemployeePrice(true)%26searchTerms%3D{query}%26anchor%3D{anchor}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D{count}&language={country_language}&localizedRangeStr=%7BlowestPrice%7D%E2%80%94%7BhighestPrice%7D'
-    html = requests.get(url=url)
+    html = requests.get(url=url, timeout=3)
     output = json.loads(html.text)
     totalPages = output['data']['products']['pages']['totalPages']
     sleep(1)
